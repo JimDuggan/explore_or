57c5bec80648922ac3c5c5d17bd8980de1115780
Jim Duggan <jim.duggan@nuigalway.ie>
2024-04-20 14:51:49 +0100

    Update Chapter10.R

--- Part II/10 purrr/src/Chapter10.R
+++ Part II/10 purrr/src/Chapter10.R
@@ -1,68 +1,51 @@
-# Chapter 1: Getting Started with R
-
-# Assign 25 to  x
-x <- 25
-# Display x
-x
-# Add 21 to x and store the result y
-y <- x + 25
-# Display y
-y
-
-
-# Call the function c() 
-# Store the result in the variable v
-v <- c(10, 20, 30)
-# Display v
-v
-
-sum(v)
-mean(v)
-sqrt(v)
-
-
-# install.packages("aimsir17")
-
-
-
+# Chapter 10 Examples -  Processing Data with purrr
+library(purrr)
 library(aimsir17)
-library(ggplot2)
+library(dplyr)
+library(repurrrsive)
 
-head(stations)
+o1 <- purrr::map(c(1,2,3,2),function(x)x^2)
+str(o1)
 
-
-p1 <- ggplot(mtcars,aes(x=disp,y=mpg))+
-  geom_point()+
-  geom_smooth(method="lm",se = F)+
-  labs(title="A simple regression example in R",
-       subtitle = "Data points for 32 automobiles showing the line of best fit",
-       x="Displacement (cubic inches)",
-       y="Miles per gallon")
+o2 <- purrr::map(c(1,2,3,2),~.x^2)
+str(o2)
+o3 <- purrr::map(c(1,2,3,2),~.^2)
+str(o3)
 
 
-X <- mtcars$disp
-X
+mtcars %>%
+  dplyr::select(mpg,cyl,disp) %>%
+  purrr::map_dbl(mean)
 
-Y <- mtcars$mpg
-Y
-
-
-mod <- lm(Y~X)
-coefficients(mod)
+sw_films %>%
+  purrr::map_chr(~.x$director) %>%
+  unique()
 
 
+means <- c(10,20,30)
+sds   <- c(2,4,7)
 
-cor(Y,X)
+purrr::map2(means,sds,~rnorm(5,.x,.y)) %>% str()
+
+d_data <- observations %>%
+  dplyr::filter(complete.cases(observations)) %>%
+  dplyr::group_by(station,month,day) %>%
+  dplyr::summarize(MaxWdsp=max(wdsp,na.rm=TRUE),
+                   DailyAverageMSL=mean(msl,na.rm=TRUE)) %>%
+  dplyr::ungroup()
+d_data
 
 
+cor7 <- d_data %>%
+  dplyr::group_by(station) %>%
+  dplyr::group_split() %>%
+  purrr::map_df(~{
+    corr <- cor(.x$MaxWdsp,.x$DailyAverageMSL)
+    tibble(Station=first(.x$station),
+           CorrCoeff=corr)
+  }) %>%
+  dplyr::arrange(CorrCoeff) %>%
+  dplyr::slice(1:7)
 
-
-
-
-
-
-
-
-
-
+cor7
 
