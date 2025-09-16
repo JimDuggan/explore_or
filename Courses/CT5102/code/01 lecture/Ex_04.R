205b2787a1928e7f2cdd696b83d1b052ed851be1
Jim Duggan <jim.duggan@nuigalway.ie>
2025-09-16 09:57:55 +0100

    Adding example 04 from lecture

---
+++ Courses/CT5102/code/01 lecture/Ex_04.R
@@ -0,0 +1,13 @@
+# Set the seed
+set.seed(99)
+
+# Generate the data
+temp <- sample(-5:30,20,replace = T)
+
+# Use vectorised ifelse to generate new
+# descriptive data on temperatures
+temp_des <- ifelse(temp<4,
+                   "Cold",
+                   ifelse(temp>25,"Hot",
+                          "Medium"))
+
