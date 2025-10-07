f59e5e017fa3a612e46dd08366259c73aec598c0
Jim Duggan <jim.duggan@nuigalway.ie>
2025-10-07 11:43:42 +0100

    Completing lecture solutions

--- Courses/CT5102/code/02 functions/sw_data.r
+++ Courses/CT5102/code/02 functions/sw_data.r
@@ -58,18 +58,55 @@ sw_heights[which(sw_names=="Luke Skywalker")]
 
 # (3) From sw_planets, extract the orbital period as a number, and display the mean and sd
 
+op <- sw_planets |>
+        lapply(function(x){
+          if(x$orbital_period == "unknown")
+            NA
+          else
+            as.double(x$orbital_period)}) |>
+         unlist() |>
+         summary()
 
 # (4) From sw_species, extract the names of those with the designation "sentient"
 
+sent <- sw_species |>
+          lapply(function(x){
+            if(x$designation=="sentient")
+              x$name
+            else
+              NA
+          }) |>
+          unlist()
+
+sent <- sent[!is.na(sent)]
+
 
 # (5) From sw_starships, extract the crew number, and display the mean and sd
+crew <- sw_starships |>
+          lapply(function(x)if(x$crew == "unknown") NA 
+                               else as.numeric(x$crew)) |>
+          unlist()
 
+m_crew <- mean(crew,na.rm=TRUE)
+sd_crew <- sd(crew,na.rm=TRUE)
+md_crew <- median(crew,na.rm=TRUE)
 
 # (6) From sw_vehicles, extract the name and the manufacturer into a list.
+
+veh <- sw_vehicles |>
+         lapply(function (x) list(x$name,x$manufacturer))
 
 # (7) Explore adding extra parameters to lapply, for example, 
 #     solving a quadratic equation ax^2+bx+c, where a, b, and c are 
 #     passed to the function
+
+a1 <- 2; b1 <- 3; c1 <- 10
+x <- seq(-100,100,.5) 
+y <- x |>
+      lapply(function(x,a,b,c)a*x^2+b*x+c,a1,b1,c1) |>
+      unlist()
+
+plot(x,y)
 
 
 
