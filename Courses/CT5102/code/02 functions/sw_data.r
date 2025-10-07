e8e129e485503bd74008dd67ffaa21f12e59ea75
Jim Duggan <jim.duggan@nuigalway.ie>
2025-10-07 10:24:34 +0100

    Adding solution for 1st problem

--- Courses/CT5102/code/02 functions/sw_data.r
+++ Courses/CT5102/code/02 functions/sw_data.r
@@ -1,7 +1,38 @@
 library(repurrrsive)
 
-# (1) From sw_films, extract the opening crawl and copy this into an atomic vector
-# of characters
+# Using lapply()
+
+# (1) From sw_films, extract the opening crawl and copy this 
+# into an atomic vector of characters
+
+crawl <- lapply(sw_films,function(x1){
+  x1$opening_crawl
+}) |> unlist()
+
+# Best solution
+crawl <- sw_films |>
+         lapply(function(x)x$opening_crawl) |>
+         unlist()
+
+get_crawl <- function(x){
+  x$opening_crawl
+}
+
+crawl2 <- sw_films |>
+  lapply(get_crawl) |>
+  unlist()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # (2) From sw_people, extract the name and height and store these as two parallel 
@@ -20,7 +51,9 @@ library(repurrrsive)
 
 # (6) From sw_vehicles, extract the name and the manufacturer into a list.
 
-
+# (7) Explore adding extra parameters to lapply, for example, 
+#     solving a quadratic equation ax^2+bx+c, where a, b, and c are 
+#     passed to the function
 
 
 
