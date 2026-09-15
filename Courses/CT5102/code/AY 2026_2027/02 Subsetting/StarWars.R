6a983d4c54234785a1fcc2c837e5199fff22125c
Jim Duggan <jim.duggan@nuigalway.ie>
2026-09-15 12:20:53 +0100

    Create StarWars.R

---
+++ Courses/CT5102/code/AY 2026_2027/02 Subsetting/StarWars.R
@@ -0,0 +1,24 @@
+# Find and George Lucas movies
+
+library(repurrrsive)
+
+gl_true <- vector(mode="logical",
+                  length=length(sw_films))
+
+for(i in seq_along(sw_films)){
+  x <- sw_films[[i]]
+  if (x$director=="George Lucas")
+    gl_true[i] <- TRUE
+  cat("Index = ",i,"\n")
+  cat("Director = ",x$director,"\n")
+  cat("Movie = ",x$title,"\n\n")
+}
+
+gl_sw_films <- sw_films[gl_true]
+
+gl_films <- vector(mode="character",
+                   length=length(gl_sw_films))
+
+for(i in seq_along(gl_sw_films)){
+  gl_films[i] <- gl_sw_films[[i]]$title
+}
